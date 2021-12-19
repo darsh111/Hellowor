e4da91fcb01d677a3cd1529a09b0c95bf1a4df6f
darsh111 <darshpatel454545@gmail.com>
2021-12-18 22:02:32 -0500

    First commit

---
+++ eclipse-workspace/myhelloworld/src/main/java/myhelloworld/hello.java
@@ -0,0 +1,14 @@
+package myhelloworld;
+
+public class hello {
+
+	
+	    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("taste hello word");
+		
+	}
+
+}
